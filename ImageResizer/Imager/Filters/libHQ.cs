300422f348247c87f7d3d158c6ee7d0c88676523
Hawkynt <Hawkynt@users.noreply.github.com>
2010-06-24 06:54:15 +0000

    +GNU license stuff

--- ImageResizer/Imager/Filters/libHQ.cs
+++ ImageResizer/Imager/Filters/libHQ.cs
@@ -1,7 +1,21 @@
 ﻿#region (c)2010 Hawkynt
 /*
- * cImage 
- * Image filtering library by Hawkynt
+ *  cImage 
+ *  Image filtering library 
+    Copyright (C) 2010 Hawkynt
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * This is a C# port of my former classImage perl library.
  * You can use and modify my code as long as you give me a credit and 
  * inform me about updates, changes new features and modification. 
