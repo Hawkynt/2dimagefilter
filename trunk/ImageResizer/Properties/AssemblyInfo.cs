f937a6f5685224b523ba5d1d158099cdf8e637f3
hawkynt@gmx.de <hawkynt@gmx.de@27afdd0b-57eb-775b-69e2-b6d69e8df1ac>
2013-08-28 08:04:48 +0000

    # filters with round brackets were not parsed correctly in scripts and CLI

--- trunk/ImageResizer/Properties/AssemblyInfo.cs
+++ trunk/ImageResizer/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.2.5")]
-[assembly: AssemblyFileVersion("1.1.2.5")]
+[assembly: AssemblyVersion("1.1.2.8")]
+[assembly: AssemblyFileVersion("1.1.2.8")]
